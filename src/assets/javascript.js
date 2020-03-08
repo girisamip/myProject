20fd82cb47304fe04bce6265c8d556c3e6f3c205
iamsamipg <girisamip5@gmail.com>
2020-03-08 00:52:36 -0600

    initial login part

---
+++ src/assets/javascript.js
@@ -0,0 +1,21 @@
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Get the modal
+var model = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(eventt) {
+  if (eventt.target == model) {
+    model.style.display = "none";
+  }
+}
+
